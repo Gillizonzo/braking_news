96d2547864582481eeaebe314eabdc692baf688c
Gillizonzo <sgill2k@gmail.com>
2025-02-05 21:03:07 -0500

    saving prompts to disk, making nanoids for vector db ids since websites have inconsistent url identifiers

--- ChromaNewsDB.py
+++ ChromaNewsDB.py
@@ -5,7 +5,11 @@ import os
 class ChromaNewsDB:
     def __init__(self, client_path='news_vector_db'):
         self.client = chromadb.PersistentClient(path=client_path)
-        self.embedding_func = embedding_functions.GoogleGenerativeAiEmbeddingFunction(api_key=os.getenv('GEMINI_API_KEY'))
+        self.embedding_func = embedding_functions.GoogleGenerativeAiEmbeddingFunction(api_key=os.getenv('GEMINI_API_KEY')) #TO DETERMINE: api "used" per call?
         self.main_collection = self.client.get_or_create_collection(name='news_summaries', embedding_function=self.embedding_func)
-
     
+    def batch_intelligent_insert(self, news_article_summary_batch, identifiers):
+        self.main_collection.upsert(
+            ids=identifiers,
+            documents=news_article_summary_batch
+        )
--- news_api_wrapping.py
+++ news_api_wrapping.py
@@ -1,5 +1,6 @@
 from newsapi import NewsApiClient
 from utils import get_content
+from nanoid import generate
 import os
 
 api = NewsApiClient(api_key=os.getenv('NEWS_API_KEY'))
@@ -14,7 +15,11 @@ def obtain_article_text_for_prompt(article_object):
     article_content = get_content(article_object['url'])
     return prompt + article_content
 
-def get_prompts_from_sources(sources):
+def get_id_prompt_pair_from_sources(sources):
     article_objects = api.get_top_headlines(sources=sources)['articles']
-    return [obtain_article_text_for_prompt(article_object) for article_object in article_objects]
+    return [(generate(), obtain_article_text_for_prompt(article_object)) for article_object in article_objects]
 
+def save_prompts_to_disk(id_prompt_pairs):
+    for nano_id, prompt in id_prompt_pairs:
+        with open(f'./prompts/{nano_id}.txt', 'w') as file:
+            file.write(prompt)
